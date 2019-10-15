81e9e258e5e55986be1b5b6a01b6fbe6448da183
Hadziq <36812793+hadziqmtqn@users.noreply.github.com>
2019-10-15 10:56:49 +0700

    Add files via upload
    File create.blade.php, edit.blade.php, index.blade.php, read.blade.php letakkan di resource/views/

---
+++ edit.blade.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Crud Laravel</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+    <a class="navbar-brand" href="/">
+    <img src="https://www.gravatar.com/avatar/62b54bac85724a5e49ba5c9f6ee825b9.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+    Hadziq
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+        <a class="nav-item nav-link" href="/create?new=Entry+Baru">Entry Baru</a>
+        <a class="nav-item nav-link" href="#">Pricing</a>
+        <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </div>
+    </div>
+    </div>
+    </nav>
+  <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h4>Tutorial CRUD Laravel</h4>
+            </div>
+            <div class="panel-body">
+                <form action="{{url('update', $data->id)}}" method="post">
+                    <!-- <input type="hidden" name="id" id="id" value="{{$data->id}}"> -->
+                    <div class="form-group">
+                        <label for="kode">Kode Buku</label>
+                        <input type="text" name="kode" id="kode" value="{{$data->kode_buku}}" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="judul">Judul Buku</label>
+                        <input type="text" name="judul" id="judul" value="{{$data->judul}}" class="form-control">
+                    </div>                    
+                    <div class="form-group">
+                        <textarea id="deskripsi" name="deskripsi" class="form-control">
+                            {!!$data->deskripsi!!}
+                        </textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="send" id="send" value="Simpan" class="btn btn-success">{!!csrf_field()!!}                       
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.blade.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Perpustakaan</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container">
+    <a class="navbar-brand" href="/">
+    <img src="https://www.gravatar.com/avatar/62b54bac85724a5e49ba5c9f6ee825b9.jpg" width="30" height="30" class="d-inline-block align-top" alt="">
+    Hadziq
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+        <a class="nav-item nav-link" href="/create?new=Entry+Baru">Entry Baru</a>
+        <a class="nav-item nav-link" href="#">Pricing</a>
+        <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </div>
+    </div>
+    </div>
+    </nav>
+  <div class="container">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h1>Daftar Nama Buku</h1>
+            </div>
+            <div class="panel-body">
+                <form action="{{url('create')}}" method="get">
+                    <div class="form-group">
+                        <input type="submit" name="new" id="new" value="Entry Baru" class="btn btn-success">
+                    </div>
+                </form>
+                <table class="table table-striped">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th>Kode Buku</th>
+                            <th>Judul</th>
+                            <th>Deskripsi</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach($data as $key => $d)
+                        <tr>
+                            <td>{{ $d->kode_buku }}</td>
+                            <td>{{ $d->judul }}</td>
+                            <td>{{ $d->deskripsi }}</td>
+                            <td>
+                                <a href="{{url('read',array($d->id))}}" class="badge badge-info">Detail</a>
+                                <a href="{{url('delete',array($d->id))}}" class="badge badge-danger">Delete</a>
+                                <a href="{{url('edit',array($d->id))}}" class="badge badge-warning">Edit</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
